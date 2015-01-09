32aa8639a9164576a7aa6726313cbb5cee4535e5
Jason Bobich <jasonbobich@gmail.com>
2015-01-09 12:57:11 -0900

    Removed themeblvd_disable_nag (see desc)
    Removed for security… This nag system was originally implemented, but
    never really used in the actual theme itself, but instead allowed other
    TB plugins to use it. All plugins on wp.org should have no been updated
    to have their own notice functions, so we can remove this from the
    theme.

--- framework/themeblvd.php
+++ framework/themeblvd.php
@@ -50,7 +50,6 @@ if ( is_admin() ) {
 	add_action( 'themeblvd_api', 'themeblvd_api_init' );
 	add_action( 'admin_enqueue_scripts', 'themeblvd_non_modular_assets' );
 	add_action( 'admin_init', 'themeblvd_add_sanitization' );
-	add_action( 'admin_init', 'themeblvd_disable_nag' );
 	add_action( 'admin_init', 'themeblvd_clear_options' );
 	add_action( 'wp_before_admin_bar_render', 'themeblvd_admin_menu_bar' );
 	add_action( 'themeblvd_options_footer_text', 'themeblvd_options_footer_text_default' );
